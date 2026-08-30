dbf6a864498a926f5747b25f97b00099a33f390c
vittorio scocca <vittorio.scocca@pynlab.com>
2026-08-30 10:53:31 +0200

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "it.vittorioscocca.kidbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "2.2.3"
+        versionCode = 47
+        versionName = "2.2.4"
         manifestPlaceholders["googleMapsApiKey"] = mapsApiKey
         buildConfigField("String", "MAPS_API_KEY", "\"$mapsApiKey\"")
         buildConfigField("Boolean", "AI_ENABLED", "true")
